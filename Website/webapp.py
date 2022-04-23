87129405367766db798e8601f18632cf232e7e1e
Tobias Plum <tobias@fam-plum.de>
2022-04-24 00:41:06 +0200

    Reformatted webapp.py

--- Website/webapp.py
+++ Website/webapp.py
@@ -6,9 +6,10 @@ port = 80
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    data = json.load(open('../data.json', 'r'))
+    data = json.load(open("../data.json", "r"))
     articlesToShow = [
         {
             "id": "1",
@@ -21,9 +22,10 @@ def index():
             "headline": "Breakiiiing News 2",
             "text": "Hello World! Lorem ipsum...",
             "url": "https://netzpolitik.org",
-        }
+        },
     ]
     return render_template("index.html", articlesToShow=data)
-    #return render_template("index.html", articlesToShow=articlesToShow)
+    # return render_template("index.html", articlesToShow=articlesToShow)
+
 
 app.run(host="0.0.0.0", port=port)
