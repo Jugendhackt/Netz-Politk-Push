605baeb99536dfc8565705f3b888b58951a2ba7a
neutroner1 <pueller2007@gmail.com>
2022-04-23 15:11:03 +0000

    converte_text

--- src/shorting_texts.py
+++ src/shorting_texts.py
@@ -1,19 +1,20 @@
 from summarizer import summarize
+import json
 
+with open ('Feed-to-unshortened-json/unshorted.json', 'r') as data:
+    jsn = json.load(data)
 
-data= data["unshorted-text"]
-
-
-def convert_text(title, text):
+def convert_text(text, title):
     return summarize(title, text, count = 2)
 
-newsletter_short= {
-    "title"           : "Titel",
-    "shorted-text"  : convert_text(Name['unshorted_text'], Name['title']),
-    "link"            : "https://*/*",
-    "date"            : "",
-    "id"              : "<replace(title, ' ', '-) + date>"
-  }
+keys = list(jsn.keys())
+
+def in_json(jsn):
+
+    keys = list(jsn.keys())
+
+print(convert_text(jsn[keys[0]]['unshorted-text'],jsn[keys[0]]['title']))
+
 
 
 
