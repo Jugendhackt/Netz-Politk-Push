68ab6a687bc6ecad0ead75e9811a50fa4ce73361
Tobias Plum <tobias@fam-plum.de>
2022-04-24 11:10:19 +0200

    Website show author name

--- Website/webapp.py
+++ Website/webapp.py
@@ -10,22 +10,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     data = json.load(open("../data.json", "r"))
-    articlesToShow = [
-        {
-            "id": "1",
-            "headline": "Breakiiiing News",
-            "text": "Hello World! Lorem ipsum...",
-            "url": "https://netzpolitik.org",
-        },
-        {
-            "id": "2",
-            "headline": "Breakiiiing News 2",
-            "text": "Hello World! Lorem ipsum...",
-            "url": "https://netzpolitik.org",
-        },
-    ]
     return render_template("index.html", articlesToShow=data)
-    # return render_template("index.html", articlesToShow=articlesToShow)
 
 
 app.run(host="0.0.0.0", port=port)
