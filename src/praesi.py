c73e00c8fbb78308b251cef28068fa48107ecc42
neutroner1 <pueller2007@gmail.com>
2022-04-24 10:49:40 +0000

    praesentation

---
+++ src/praesi.py
@@ -0,0 +1,44 @@
+import json
+import requests
+
+url = ""
+try:
+    url = os.environ["URL"]
+except:
+    pass
+
+
+data_url = 'data/data.json'
+
+def read_data(data_url):
+    with open (data_url, 'r') as data:
+        jsn = json.load(data)
+    keys = list(jsn.keys())
+    for i in range(len(keys)):
+        ndata = jsn[keys[i]]
+        send_notification(ndata, keys)
+
+
+
+def send_notification(data, id):
+    text = "shortened-text"
+    link = data["link"]
+    if(len(data["unshortened-text"]) < len(data["shortened-text"])
+            or len(data["unshortened-text"]) < 250):
+        text = "unshortened-text"
+    if len(url) > 8:
+        link = url + "#" + id
+    requests.post("https://ntfy.sh/Netz-Politik-News-Push",
+                  data=data[text].encode("utf-8"),
+                  headers={
+                          "Title": data["title"].encode("utf-8"),
+                          "Priority": "urgent",
+                          "Tags": "warning",
+                          "Click": link.encode("utf-8"),
+                          "date": data["date"].encode("utf-8")
+                      })
+
+
+
+print(read_data(data_url))
+send_notification()
