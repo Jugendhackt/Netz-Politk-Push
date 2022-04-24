5ed38d6618314adf5249e20e2cfc9987e1d88328
Tobias Plum <tobias@fam-plum.de>
2022-04-24 18:17:01 +0200

    Website only display 10 articles

--- Website/webapp.py
+++ Website/webapp.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 import json
+import csv
 
 port = 8080
 
@@ -9,8 +10,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    idsToShow = []
+    for line in csv.reader(open("../data/sorted_keys.csv", "r")):
+        idsToShow.append(line[1])
+        if len(idsToShow) == 10:
+            break
     data = json.load(open("../data.json", "r"))
-    return render_template("index.html", articlesToShow=data)
+    return render_template("index.html", idsToShow=idsToShow, allArticles=data)
 
 
 app.run(host="0.0.0.0", port=port)
